d1b0b1397c082b973c903bb4914f28247ae8ae1a
lgomez9 <gmezleo97@gmail.com>
2022-03-04 21:23:20 +0000

    Added unit tests using assertNotEqual()

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -8,6 +8,7 @@ class TestEnglishToFrench(unittest.TestCase):
         self.assertEqual(english_to_french('My name is Leo'), 'Mon nom est Leo')
         self.assertEqual(english_to_french(''), '')
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
+        self.assertNotEqual(english_to_french('Hello'), 'Au revoir')
 
 class TestFrenchToEnglish(unittest.TestCase):
     def test1(self):
@@ -15,5 +16,6 @@ class TestFrenchToEnglish(unittest.TestCase):
         self.assertEqual(french_to_english('Mon nom est Leo'), 'My name is Leo')
         self.assertEqual(french_to_english(''), '')
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
+        self.assertNotEqual(french_to_english('Au revoir'), 'Hello')
 
 unittest.main()
